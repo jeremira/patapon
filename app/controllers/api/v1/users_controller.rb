65f1bf0384123939c87142ef37233cbc785d1f8a
jeremira <jmiraille@gmail.com>
2019-02-24 14:24:44 +0100

    add email validation on user controlelr

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,11 @@ module Api
   module V1
     class UsersController < ActionController::API
       def create
-        render json: "ok", status: 200
+        if params[:email].present?
+          render json: {error: nil}, status: 200
+        else
+          render json: {error: "An email is required."}, status: 422
+        end
       end
     end
   end
--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,12 +1,56 @@
 require "rails_helper"
 
 RSpec.describe "User Create", :type => :request do
+  let(:parsed_body) {JSON.parse(response.body).symbolize_keys}
+
+  before :each do
+    post api_v1_users_path(user_params)
+  end
 
   describe "With valid params" do
-    
+    let(:user_params) {{email: "test1@babar.com"}}
+
     it "render 200" do
-      post api_v1_users_path
       expect(response.status).to eq 200
+    end
+    it "has a no error message" do
+      expect(parsed_body[:error]).to be nil
+    end
+  end
+
+  describe "with invalid params" do
+
+    context "with no email" do
+      let(:user_params) {nil}
+
+      it "render 422" do
+        expect(response.status).to eq 422
+      end
+      it "has a valid error message" do
+        expect(parsed_body[:error]).to eq "An email is required."
+      end
+    end
+
+    context "with a blank email" do
+      let(:user_params) {{email: ""}}
+
+      it "render 422" do
+        expect(response.status).to eq 422
+      end
+      it "has a valid error message" do
+        expect(parsed_body[:error]).to eq "An email is required."
+      end
+    end
+
+    context "with a nil email" do
+      let(:user_params) {{email: nil}}
+
+      it "render 422" do
+        expect(response.status).to eq 422
+      end
+      it "has a valid error message" do
+        expect(parsed_body[:error]).to eq "An email is required."
+      end
     end
   end
 end
