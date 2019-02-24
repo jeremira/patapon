8884ccd37f477f50f7b7d631f8e02a7f0b519676
jeremira <jmiraille@gmail.com>
2019-02-24 17:06:19 +0100

    add mirroring method

--- lib/mirroring/user/create.rb
+++ lib/mirroring/user/create.rb
@@ -8,17 +8,31 @@ module Mirroring
       end
 
       def mirror_and_save_user
-
+        if @internal_user.valid?
+          return false if mirroring_user_failed?
+          @internal_user.save
+          @internal_user
+        end
       end
 
       #
       # List all supported external services where User will be created
       #
       def supported_mirrors
-         [:hubspot]
+         [Hubspot]
       end
 
       private
+
+      def mirroring_user_failed?
+        process_external_mirroring.include? false
+      end
+
+      def process_external_mirroring
+        supported_mirrors.map do |mirror|
+          mirror.create_user(email: @internal_user.email)
+        end
+      end
 
       def create_and_mirror_user
         # build user
--- spec/lib/mirroring/user/create_spec.rb
+++ spec/lib/mirroring/user/create_spec.rb
@@ -39,21 +39,26 @@ describe "Mirroring::User::Create" do
       expect(user_creator.supported_mirrors).to be_an Array
     end
     it "return all external services supported" do
-      expect(user_creator.supported_mirrors).to eq([:hubspot])
+      expect(user_creator.supported_mirrors).to eq([Hubspot])
     end
   end
 
   describe "#mirror_and_save_user" do
-    class self::Babar
-      def self.create_user ; end
-    end
     let(:on_try_fonction) {user_creator.mirror_and_save_user}
+    #
+    # Stub class for external services
+    #
+    class self::Babar
+      def self.create_user(email:) ; end
+    end
 
     before :each do
-      allow(user_creator).to receive(:supported_mirrors) {[:babar]}
+      allow(self.class::Babar).to receive(:create_user) {stubbed_external_answer}
+      allow(user_creator).to receive(:supported_mirrors) {[self.class::Babar]}
     end
 
     context "for an invalid internal user" do
+      let(:stubbed_external_answer) {nil}
       let(:user_params) {{email: nil}}
 
       it "do not save internal user" do
@@ -63,7 +68,46 @@ describe "Mirroring::User::Create" do
         expect(self.class::Babar).not_to receive(:create_user)
         on_try_fonction
       end
+      it "return nil" do
+        expect(on_try_fonction).to be nil
+      end
+    end
 
+    context "for a valid user" do
+      let(:user_params) {{email: "babar@test.com"}}
+
+      context "and a valid external call" do
+        let(:stubbed_external_answer) {true}
+
+        it "do save internal user" do
+          expect{on_try_fonction}.to change(::User, :count).by 1
+        end
+        it "do mirror User in Babar services" do
+          expect(self.class::Babar).to receive(:create_user).with(email: "babar@test.com")
+          on_try_fonction
+        end
+        it "return User instance" do
+          expect(on_try_fonction).to be_an User
+        end
+        it "save user instance" do
+          expect(on_try_fonction.persisted?).to be true
+        end
+      end
+
+      context "but an invalid external call" do
+        let(:stubbed_external_answer) {false}
+
+        it "do not save internal user" do
+          expect{on_try_fonction}.not_to change(User, :count)
+        end
+        it "do mirror User in Babar services" do
+          expect(self.class::Babar).to receive(:create_user).with(email: "babar@test.com")
+          on_try_fonction
+        end
+        it "return nil" do
+          expect(on_try_fonction).to be false
+        end
+      end
     end
   end
 end
