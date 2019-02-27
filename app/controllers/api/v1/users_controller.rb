02b46682567fe12672bcf9674e5f15fda7f92c9e
jeremira <jmiraille@gmail.com>
2019-02-27 10:03:12 +0100

    brush up style and specs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,10 @@ module Api
     class UsersController < ActionController::API
       def create
         if params[:email].present?
-          UserWorker.perform_async(email: params[:email], note: params[:note])
+          UserWorker.perform_async(
+            email: params[:email],
+            note: params[:note]
+          )
           render json: {error: nil}, status: 200
         else
           render json: {error: "An email is required."}, status: 422
@@ -12,4 +15,3 @@ module Api
     end
   end
 end
-#todo dont forget strong params
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
 
-  it "inherit from ApplicationController" do
+  it "inherit from ActionController::API" do
     expect(described_class).to be < ActionController::API
   end
 end
